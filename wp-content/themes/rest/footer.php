2a4016ea688b4e4724926c160af95ee2656d0fed
Renan Gomes <renanoliveirae10@gmail.com>
2024-11-14 23:57:23 -0300

    Começando a colocar páginas nos templates do wordpress, usando o loop do have_posts

--- wp-content/themes/rest/footer.php
+++ wp-content/themes/rest/footer.php
@@ -2,8 +2,8 @@
 	<nav class="nav-footer">
 		<ul>
 			<li class="current_page_item"><a href="index.html">Menu</a></li>
-			<li><a href="sobre.html">Sobre</a></li>
-			<li><a href="contato.html">Contato</a></li>
+			<li><a href="sobre">Sobre</a></li>
+			<li><a href="contato">Contato</a></li>
 		</ul>
 	</nav>
 
